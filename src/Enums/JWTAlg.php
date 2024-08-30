5af31a44954916c33de3fe0a9f44281e70d1b80a
vitor-hugo <vitor83dev@gmail.com>
2024-08-30 16:18:35 -0300

    refact: transforming enum values to lowercase

--- src/Enums/JWTAlg.php
+++ src/Enums/JWTAlg.php
@@ -7,10 +7,10 @@ namespace Torugo\JWT\Enums;
  */
 enum JWTAlg: string
 {
-    case HS256 = "HS256";
-    case HS384 = "HS384";
-    case HS512 = "HS512";
-    case RS256 = "RS256";
-    case RS384 = "RS384";
-    case RS512 = "RS512";
+    case HS256 = "hs256";
+    case HS384 = "hs384";
+    case HS512 = "hs512";
+    case RS256 = "rs256";
+    case RS384 = "rs384";
+    case RS512 = "rs512";
 }
